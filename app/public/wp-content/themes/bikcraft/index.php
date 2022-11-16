dace62012f46a3119b165ba063ce7df0905da8fb
Raphael Araujo <araujowebdev@gmail.com>
2022-11-16 17:09:08 -0300

    add loop to pages

--- app/public/wp-content/themes/bikcraft/index.php
+++ app/public/wp-content/themes/bikcraft/index.php
@@ -1,5 +1,10 @@
 <?php get_header(); ?>
 
-  <p>Ooops!</p>
+  <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    <h1><?php the_title(); ?></h1>
+    <?php the_content(); ?>
+  <?php endwhile; else: ?>
+    <p><?php _e('Descupe, nenhum post com seu(s) critério(s) encontrado(s)!') ?></p>
+  <?php endif;  ?>
 
 <?php get_footer() ?>
