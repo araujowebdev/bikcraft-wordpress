0a681ab89978b94aae272a87e64c729e153a18cc
Raphael Araujo <araujowebdev@gmail.com>
2022-11-16 18:46:20 -0300

    install advanced custom fields plugin

--- app/public/wp-content/themes/bikcraft/page-portfolio.php
+++ app/public/wp-content/themes/bikcraft/page-portfolio.php
@@ -45,34 +45,12 @@
   </section>
 
   <section class="portfolio">
-    <div class="container">
-      <ul class="portfolio_lista rslides_portfolio">
-        <li>
-          <div class="grid-8">
-            <img src="img/portfolio/retro.jpg" alt="Bicicleta Retrô" />
-          </div>
-          <div class="grid-8">
-            <img src="img/portfolio/passeio.jpg" alt="Bicicleta Passeio" />
-          </div>
-          <div class="grid-16">
-            <img src="img/portfolio/esporte.jpg" alt="Bicicleta Esporte" />
-          </div>
-        </li>
+  <div class="container">
+  <?php include(TEMPLATEPATH . "/inc/clientes-portfolio.php"); ?>
+  </div>
+</section>
 
-        <li>
-          <div class="grid-8">
-            <img src="img/portfolio/passeio.jpg" alt="Bicicleta Passeio" />
-          </div>
-          <div class="grid-8">
-            <img src="img/portfolio/retro.jpg" alt="Bicicleta Retrô" />
-          </div>
-          <div class="grid-16">
-            <img src="img/portfolio/esporte.jpg" alt="Bicicleta Esporte" />
-          </div>
-        </li>
-      </ul>
-    </div>
-  </section>
+
 <?php endwhile; else: endif?>
 
 <?php get_footer() ?>
