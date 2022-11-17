ccc91d7a3f4e024e575153d1d40e6c5803527c5e
Raphael Araujo <araujowebdev@gmail.com>
2022-11-17 19:42:13 -0300

    fix bugs

--- app/public/wp-content/themes/bikcraft/functions.php
+++ app/public/wp-content/themes/bikcraft/functions.php
@@ -1,30 +1,31 @@
-<?php
-  // Função para registrar os Scripts e o CSS
-  function origamid_scripts() {
-  // Desregistra o jQuery do Wordpress
-  wp_deregister_script('jquery');
+<?php 
 
-  // Registra o jQuery Novo
-  wp_register_script( 'jquery', get_template_directory_uri() . '/js/libs/jquery-1.11.2.min.js', array(), "1.11.2", true );
+// Função para registrar os Scripts e o CSS
+function origamid_scripts() {
+	// Desregistra o jQuery do Wordpress
+	wp_deregister_script('jquery');
 
-  // Registrar Plugins
-  wp_register_script( 'plugins-script', get_template_directory_uri() . '/js/plugins.js', array( 'jquery' ), false, true );
+	// Registra o jQuery Novo
+	wp_register_script( 'jquery', get_template_directory_uri() . '/js/libs/jquery-1.11.2.min.js', array(), "1.11.2", true );
 
-  // Registrar Main
-  wp_register_script( 'main-script', get_template_directory_uri() . '/js/main.js', array( 'jquery', 'plugins-script' ), false, true );
+	// Registrar Plugins
+	wp_register_script( 'plugins-script', get_template_directory_uri() . '/js/plugins.js', array( 'jquery' ), false, true );
 
-  // Registrar Modernizr
-  wp_register_script( 'modernizr', get_template_directory_uri() . '/js/libs/modernizr.custom.45655.js', array(), "45655", false );
+	// Registrar Main
+	wp_register_script( 'main-script', get_template_directory_uri() . '/js/main.js', array( 'jquery', 'plugins-script' ), false, true );
 
-  // Carrega o Script
-  wp_enqueue_script( 'modernizr' );
-  wp_enqueue_script( 'main-script' );
+	// Registrar Modernizr
+	wp_register_script( 'modernizr', get_template_directory_uri() . '/js/libs/modernizr.custom.45655.js', array(), "45655", false );
+
+	// Carrega o Script
+	wp_enqueue_script( 'modernizr' );
+	wp_enqueue_script( 'main-script' );	
 }
 add_action( 'wp_enqueue_scripts', 'origamid_scripts' );
 
 function origamid_css() {
-  wp_register_style( 'origamid-style', get_template_directory_uri() . '/style.css', array(), false, false );
-  wp_enqueue_style( 'origamid-style' );
+	wp_register_style( 'origamid-style', get_template_directory_uri() . '/style.css', array(), false, false );
+	wp_enqueue_style( 'origamid-style' );
 }
 add_action( 'wp_enqueue_scripts', 'origamid_css' );
 
@@ -51,43 +52,45 @@ add_action( 'init', 'register_my_menu' );
 
 // Custom Images Sizes
 function my_custom_sizes() {
-  add_image_size('large', 1400, 380, true);
-  add_image_size('medium', 768, 380, true);
+	add_image_size('large', 1400, 380, true);
+	add_image_size('medium', 768, 380, true);
 }
 add_action('after_setup_theme', 'my_custom_sizes');
 
 // Custom Post Type
 
 function custom_post_type_produtos() {
-  register_post_type('produtos', array(
-    'label' => 'Produtos',
-    'description' => 'Produtos',
-    'public' => true,
-    'show_ui' => true,
-    'show_in_menu' => true,
-    'capability_type' => 'post',
-    'map_meta_cap' => true,
-    'hierarchical' => false,
-    'rewrite' => array('slug' => 'produtos', 'with_front' => true),
-    'query_var' => true,
-    'supports' => array('title', 'editor', 'page-attributes','post-formats'),
+	register_post_type('produtos', array(
+		'label' => 'Produtos',
+		'description' => 'Produtos',
+		'public' => true,
+		'show_ui' => true,
+		'show_in_menu' => true,
+		'capability_type' => 'post',
+		'map_meta_cap' => true,
+		'hierarchical' => false,
+		'rewrite' => array('slug' => 'produtos', 'with_front' => true),
+		'query_var' => true,
+		'supports' => array('title', 'editor', 'page-attributes','post-formats'),
 
-    'labels' => array (
-      'name' => 'Produtos',
-      'singular_name' => 'Produto',
-      'menu_name' => 'Produtos',
-      'add_new' => 'Adicionar Novo',
-      'add_new_item' => 'Adicionar Novo Produto',
-      'edit' => 'Editar',
-      'edit_item' => 'Editar Produto',
-      'new_item' => 'Novo Produto',
-      'view' => 'Ver Produto',
-      'view_item' => 'Ver Produto',
-      'search_items' => 'Procurar Produtos',
-      'not_found' => 'Nenhum Produto Encontrado',
-      'not_found_in_trash' => 'Nenhum Produto Encontrado no Lixo',
-    )
-  ));
+		'labels' => array (
+			'name' => 'Produtos',
+			'singular_name' => 'Produto',
+			'menu_name' => 'Produtos',
+			'add_new' => 'Adicionar Novo',
+			'add_new_item' => 'Adicionar Novo Produto',
+			'edit' => 'Editar',
+			'edit_item' => 'Editar Produto',
+			'new_item' => 'Novo Produto',
+			'view' => 'Ver Produto',
+			'view_item' => 'Ver Produto',
+			'search_items' => 'Procurar Produtos',
+			'not_found' => 'Nenhum Produto Encontrado',
+			'not_found_in_trash' => 'Nenhum Produto Encontrado no Lixo',
+		)
+	));
 }
 add_action('init', 'custom_post_type_produtos');
+
+
 ?>
