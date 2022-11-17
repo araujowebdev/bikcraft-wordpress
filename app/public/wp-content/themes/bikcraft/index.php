d3850c57adaa3a51cb31c6e534c04845ee9f2cea
Raphael Araujo <araujowebdev@gmail.com>
2022-11-17 18:32:00 -0300

    create blog

--- app/public/wp-content/themes/bikcraft/index.php
+++ app/public/wp-content/themes/bikcraft/index.php
@@ -1,10 +1,29 @@
 <?php get_header(); ?>
 
-  <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-    <h1><?php the_title(); ?></h1>
-    <?php the_content(); ?>
-  <?php endwhile; else: ?>
-    <p><?php _e('Descupe, nenhum post com seu(s) critério(s) encontrado(s)!') ?></p>
-  <?php endif;  ?>
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-<?php get_footer() ?>
+<article>
+  <section class="introducao-interna introducao-geral">
+    <div class="container">
+      <h1><?php the_title(); ?></h1>
+    </div>
+  </section>
+
+  <section class="container conteudo-geral">
+    <div class="grid-8">
+      <?php the_content(); ?>
+    </div>
+  </section>
+</article>
+
+<?php endwhile; else: ?>
+
+  <section class="introducao-interna introducao-geral">
+    <div class="container">
+      <h1>Página não encontrada.</h1>
+    </div>
+  </section>
+
+<?php endif; ?>
+
+<?php get_footer(); ?>
